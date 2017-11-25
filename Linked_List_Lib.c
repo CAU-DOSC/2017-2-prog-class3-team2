2f39d4d8878509cbeb19854a5cf425c2f1728e17
SeongYunKim <cauboy17@naver.com>
2017-11-25 12:38:47 +0900

    Function First Commit

---
+++ Linked_List_Lib.c
@@ -0,0 +1,34 @@
+#include "Linked_List_Head.h"
+
+LINK creatNODE(int num)
+{
+	LINK cur;
+	cur = (LINK)malloc(sizeof(NODE));
+	cur->value = num;
+	cur->next = NULL;
+	return cur;
+}
+
+LINK appendNODE(LINK head, LINK cur)
+{
+	LINK nextnode = head;
+	if (head == NULL)
+	{
+		head = cur;
+		return head;
+	}
+	while (nextnode->next != NULL)
+		nextnode = nextnode->next;
+	nextnode->next = cur;
+	return head;
+}
+
+void printNODE(LINK head)
+{
+	LINK nextnode = head;
+	while (nextnode != NULL)
+	{
+		printf("%d ", nextnode->value);
+		nextnode = nextnode->next;
+	}
+}
