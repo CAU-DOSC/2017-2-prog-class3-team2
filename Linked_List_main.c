451aa5757b1716fd3d41781d3e6d18fdb1e26f0a
SeongYunKim <cauboy17@naver.com>
2017-11-28 02:10:10 +0900

    Solve Complie Error and Modify Prompt

--- Linked_List_main.c
+++ Linked_List_main.c
@@ -17,14 +17,14 @@ int main()
 		numofNODE++;
 	}
 
-	printf("입력된 자료의 개수 %d\n", numofNODE);
+	printf("입력된 자료의 개수: %d\n", numofNODE);
 
 	//역순으로 출력
-	printf("역순으로 출력: ");
-	reverseprintNODE(head,numofNODE);
+	printf("입력된 자료를 역순으로 출력: ");
+	reverseprintNODE(head, curnode, numofNODE);
 
 	//중간값출력
-	printf("\n중간값 출력: ");
+	printf("\n입력자료의 중간위치에 있는 자료의 값 출력: ");
 	curnode = head;
 	int mid = 0;
 	while (mid < numofNODE / 2)
@@ -37,13 +37,14 @@ int main()
 	printf("%d\n", curnode->value);
 
 	//입력순으로출력
+	printf("입력자료를 입력 순으로 출력: ");
 	printNODE(head);
 
 	//홀수번째 자료 삭제
 	head = head->next;
 	curnode = head;
 	int cur = 2;
-
+	int last;
 	if (numofNODE % 2 == 0)
 		last = numofNODE;
 	else
@@ -57,10 +58,8 @@ int main()
 	}
 	curnode->next = NULL;
 
-	printf("\n남은것출력: ");
+	printf("\n입력자료에서 홀수번째 자료를 모두 삭제 후 순서대로 출력: ");
 	printNODE(head);
-
-
-
+	puts("");
 	return 0;
 }
