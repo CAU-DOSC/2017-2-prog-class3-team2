434b446fa26851fa449927dd4defa114ce5ab9bd
celi1004 <33647623+celi1004@users.noreply.github.com>
2017-11-28 00:01:17 +0900

    Update Linked_List_Head.h

--- Linked_List_Head.h
+++ Linked_List_Head.h
@@ -18,4 +18,4 @@ typedef NODE* LINK;
 LINK createNODE(int num);
 LINK appendNODE(LINK head, LINK cur);
 void printNODE(LINK head);
-void reverseprintNODE(int last);
+void reverseprintNODE(LINK curNODE, int last);
