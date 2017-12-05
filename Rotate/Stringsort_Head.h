643eac199809fc5a99a08e7238a9abc9f0a4738a
ClearT <clear1014@gmail.com>
2017-12-05 13:16:32 +0900

    add function prototype

--- Rotate/Stringsort_Head.h
+++ Rotate/Stringsort_Head.h
@@ -8,3 +8,5 @@ char *trivial(char str[], int n, int d);
 char *juggling(char str[], int n, int d);
 char *blockswap(char str[], int n, int d);
 char *reverse(char str[], int n, int d);
+void strswap(char *str1, char *str2, int d);
+void strrev(char str[], int length);
