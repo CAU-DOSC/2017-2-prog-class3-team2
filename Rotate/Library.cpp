694f0589c4a0b7d9552e948a78931330583dc31d
ClearT <clear1014@gmail.com>
2017-12-05 13:01:36 +0900

    Update Library.cpp

--- Rotate/Library.cpp
+++ Rotate/Library.cpp
@@ -58,7 +58,6 @@ char *juggling(char str[], int n, int d)
 
 	return str;
 }
-//0 < d < strlen(str)이어야만 합니다
 
 //str1의 주소값과 str2의 주소값에서 각각 d만큼을 서로 스왑함
 void strswap(char *str1, char *str2, int d)
@@ -77,8 +76,11 @@ void strswap(char *str1, char *str2, int d)
 //str은 앞 문자열 맨 앞의 주소값, len은 문자열의 길이, d는 앞부분 문자열의 길이
 char* blockswap(char str[], int len, int d)
 {
-	//범위가 넘어가면 일단 리턴. 미완성
-	if (*(str + d) == 0)
+	if (d < 0) d = len + d;	//d가 0보다 작으면counterclockwise
+	
+	if (d > len) d = d % len;	//d > n이면 d값 수정
+	
+	if (*(str + d) == 0 || d == 0)
 		return str;
 
 	//앞 문자열과 뒤 문자열의 길이가 같을 때
